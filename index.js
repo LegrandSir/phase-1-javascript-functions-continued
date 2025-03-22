d438d6bb3b9766ed3d623aa46af6c04334dd17d3
Paul Wafula <paulwafula391@gmail.com>
2025-03-22 09:50:57 +0300

    functions continued

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+const mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+function wrapAdjective(thaplug="*"){
+    return function innerfunction(bigx = "special") {
+        return `You are ${thaplug}${bigx}${thaplug}!`;
+    };
+}
